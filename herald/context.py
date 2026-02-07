79f01bc8b002e569bf3e8e4a367dd5858e8d932c
Varun Vijaykumar <varun.vijaykumar75@gmail.com>
2026-02-07 19:24:22 +0100

    Add option for CV to be provided via ENV variable

--- herald/context.py
+++ herald/context.py
@@ -1,24 +1,32 @@
 """File containing all the context classes for the herald package."""
 import os
 import pymupdf4llm
+import dotenv
 
 
 class PromptOptions:
     """Class containing PromptOptions"""
 
     @staticmethod
-    def prepare_pdf_content(cv_pdf_file: str) -> str:
-        """
-        Prepare CV pdf content for the prompt.
+    def prepare_pdf_content(cv_pdf_file: str = None) -> str:
+        """Prepare CV pdf content for the prompt.
+
+        .. note:: 
+            CV file can be loaded via Environment var ``CV_PATH``
         
-        :param str cv_pdf_file: PDF file with CV content
+        :param str cv_pdf_file: PDF file with CV content, optional
         :return: CV content string
         :rtype: str
         """
+        # if cv_pdf_file is not set, then an attempt is made to read from the env variables
+        if cv_pdf_file is None:
+            cv_pdf_file = os.getenv("CV_PATH")
+
         if not os.path.exists(cv_pdf_file):
             raise ValueError(
                 f"The CV pdf {cv_pdf_file} does not exist! Please provide a valid one."
             )
+        
         return pymupdf4llm.to_markdown(cv_pdf_file)
 
     @staticmethod
