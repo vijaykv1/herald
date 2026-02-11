4564089fa3e1426a14ced859e51e6831a9f2719c
varunvijaykumar <varun.vijaykumar@bmwgroup.com>
2026-02-11 21:37:46 +0100

    Black all cv parsers

--- herald/cv_parser/iparser.py
+++ herald/cv_parser/iparser.py
@@ -1,17 +1,18 @@
 """Parser interface for CV parsing in herald system."""
+
 import abc
 
 
 class CVParserInterface(abc.ABC):
     """Abstract base class for CV parsers."""
-    
+
     def __init__(self, cv: str):
         """Initialize the parser with the CV content.
-        
-        .. note:: 
-            
-            Only accepts a file which is either a markdown or a text file, 
-            or a string content of the CV itself. 
+
+        .. note::
+
+            Only accepts a file which is either a markdown or a text file,
+            or a string content of the CV itself.
             The parser will determine the type based on the content.
 
         :param str cv: The CV content as a string
